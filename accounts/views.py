cffd9a88d3f8939560952aac9604df74fff1bfbc
Yair <yair2321@gmail.com>
2021-07-06 20:24:01 +0300

    set tables

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from .models import Product
+from .models import *
 
 
 def home(request):
-    return render(request, 'accounts/dashboad.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers =Customer.objects.all().count()
+    total_orders =Order.objects.all().count()
+    delivered = Order.objects.filter(status="Delivered").count()
+    pending = Order.objects.filter(status="Pending").count()
+    context = {"orders": orders, "customers": customers, "total_customers": total_customers,
+     "total_orders": total_orders, "delivered": delivered, "pending": pending }
+    return render(request, 'accounts/dashboad.html', context)
 
 def products(request):
     products = Product.objects.all()
