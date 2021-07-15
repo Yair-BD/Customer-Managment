0c958f91ef4ba26d5d29244048f17b128bc31326
Yair Ben David <79565335+Yair-BD@users.noreply.github.com>
2021-07-15 13:35:39 +0300

    set models

--- accounts/models.py
+++ accounts/models.py
@@ -45,8 +45,8 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered','Delivered')
     )
-    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_create = models.DateTimeField(auto_now_add=True, null=True)
     date_update = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
